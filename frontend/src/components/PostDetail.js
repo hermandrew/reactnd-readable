2a68b43ea982ace428ee846ec0fbdfc2adcb7719
Andrew Herman <andrew@frenchgirlsapp.com>
2018-03-04 12:33:38 -0800

    bugfix: No longer viewing deleted posts.

--- frontend/src/components/PostDetail.js
+++ frontend/src/components/PostDetail.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react'
-import { Feed, Divider, Container } from 'semantic-ui-react'
+import { Feed, Divider, Container, Icon, Header } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import PostEvent from './PostEvent'
 import {createComment, getComments} from "../actions/comments";
 import AddCommentForm from './AddCommentForm'
 import CommentList from './CommentList'
-import { get } from 'lodash'
+import { get, isEmpty } from 'lodash'
 
 class PostDetail extends Component {
   handleClose = () => {
@@ -19,17 +19,28 @@ class PostDetail extends Component {
   render() {
     const { post, comments, match: { params: { post_id = null } }} = this.props
 
-    return <Container style={{backgroundColor: 'white', padding: '2em'}}>
-      <Feed>
-        <PostEvent post={post}/>
-      </Feed>
+    if (post.deleted || isEmpty(post)) {
+      return <Container style={{backgroundColor: 'white', padding: '2em'}}>
+        <Header as='h2' icon textAlign='center'>
+          <Icon name='frown' color='red' />
+          <Header.Content>
+            Sorry Bruh!  This isn't what you're looking for...nothing here!
+          </Header.Content>
+        </Header>
+      </Container>
+    } else {
+      return <Container style={{backgroundColor: 'white', padding: '2em'}}>
+        <Feed>
+          <PostEvent post={post}/>
+        </Feed>
 
-      <CommentList comments={comments}/>
-      <div>
-        <Divider horizontal>Add Comment</Divider>
-        <AddCommentForm postID={post_id} />
-      </div>
-    </Container>
+        <CommentList comments={comments}/>
+        <div>
+          <Divider horizontal>Add Comment</Divider>
+          <AddCommentForm postID={post_id} />
+        </div>
+      </Container>
+    }
   }
 }
 
